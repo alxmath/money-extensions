743eaa79cf8ff52127e1cc3573916964ca0caac1
Alex Santos <alxmath@gmail.com>
2025-06-10 09:30:32 -0300

    Finalizado a primeira versão

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Tests;
+
+[TestClass]
+public sealed class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToint()
+    {
+        var valor = 279.98M;
+        var cents = valor.ToCents();
+        
+        Assert.AreEqual(27998, cents);
+    }
+}
